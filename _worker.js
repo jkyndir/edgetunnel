593008f607ba3710aae90e8e2c00c7807af1eb1b
Jkyn Vrsma <57865763+jkyndir@users.noreply.github.com>
2025-09-18 22:24:49 +0800

    Update _worker.js with UUID

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 ﻿
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
+let userID = '04240e1d-ba4e-41cf-b634-ca3cb29f00ad';
 let proxyIP = '';
 let DNS64Server = '';
 //let sub = '';
@@ -7175,4 +7175,5 @@ function config_Html(token = "test", proxyhost = "") {
 </html>`;
 
     return html;
+
 }
